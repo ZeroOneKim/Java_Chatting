c918039cfcbfe64041b09f2357dd56092c6cab87
YungIl Kim <wowwow0124@naver.com>
2022-11-07 18:38:56 +0900

    Main Running

---
+++ Chat_Client/Client_view.java
@@ -0,0 +1,9 @@
+package Chat_Client;
+
+import Chat_Client.service.Client_Login;
+
+public class Client_view {
+    public static void main(String[] args) {
+        Client_Login client_login = new Client_Login();
+    }
+}
